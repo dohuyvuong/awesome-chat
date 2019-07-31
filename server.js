3a6ec7ec799ca6c74e50aaa1589f3c70ab4f808b
Vuong Huy Do <dohuy.vuong@gmail.com>
2019-08-01 00:09:06 +0700

    [1.0.0] Init HelloWorld.

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+
+let app = express();
+
+let hostname = "localhost";
+let port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(port, hostname, ()=> {
+  console.log(`Hello, The Server is running at http://${hostname}:${port}/`);
+});
