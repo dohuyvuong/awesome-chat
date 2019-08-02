61fddbf7ce4bcbff8f11d99bc11f1b451faa60c2
Vuong Huy Do <dohuy.vuong@gmail.com>
2019-08-03 01:29:03 +0700

    [1.0.0] Connect and Create new item MongoDB.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 import express from "express";
+import connectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
 
 let app = express();
 
-let hostname = "localhost";
-let port = 8000;
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(port, hostname, ()=> {
-  console.log(`Hello, The Server is running at http://${hostname}:${port}/`);
+app.get("/test-db", async (req, res) => {
+  try {
+    let item = {
+      userId: "10000",
+      contactId: "10001"
+    };
+
+    let contact = await ContactModel.createNew(item);
+
+    res.send(contact);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=> {
+  console.log(`Hello, The Server is running at http://${process.env.APP_HOST}:${process.env.APP_PORT}/`);
 });
